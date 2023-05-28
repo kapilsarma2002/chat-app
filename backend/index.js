adee33aafb0a2d6f775f7f0fb20a60982d4580ff
kapil <sarmakapil1818@gmail.com>
2023-05-28 19:09:48 +0530

    frontend-init

--- backend/index.js
+++ backend/index.js
@@ -6,8 +6,23 @@ app.use(express.json());
 app.use(cors({ origin: true }));
 
 app.post("/authenticate", async (req, res) => {
-  const { username } = req.body;
-  return res.json({ username: username, secret: "sha256..." });
+  const axios = require("axios");
+
+  app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+    // Get or create user on Chat Engine!
+    try {
+      // @ts-ignore
+      const r = await axios.put(
+        "https://api.chatengine.io/users/",
+        { username: username, secret: username, first_name: username },
+        { headers: { "Private-Key": "068a2e46-637d-4ddc-9142-7be478f272f8" } }
+      );
+      return res.status(r.status).json(r.data);
+    } catch (e) {
+      return res.status(e.response.status).json(e.response.data);
+    }
+  });
 });
 
 app.listen(3001);
